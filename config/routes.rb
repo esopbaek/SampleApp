0c2ce32a0e7c2a619277e5e37d477fdcee8cf5e4
App Academy Student <student@appacademy.io>
2014-08-04 03:51:45 -0400

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 SampleApp::Application.routes.draw do
   root  'static_pages#home'
-
+  resources :sessions, only: [:new, :create, :destroy]
 
   resources :users
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   match '/signup',  to: 'users#new',         via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
