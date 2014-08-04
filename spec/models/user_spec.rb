0ed774e5050cd2b38550559d30766d4ff8b69e92
App Academy Student <student@appacademy.io>
2014-08-04 01:48:09 -0400

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -66,4 +66,14 @@ describe User do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
   end
+
+  describe "email addresses with mixed cases for emails" do
+    let(:mixed_case_email) { "foObaR@example.com" }
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email). to eq mixed_case_email.downcase
+    end
+  end
+
 end
